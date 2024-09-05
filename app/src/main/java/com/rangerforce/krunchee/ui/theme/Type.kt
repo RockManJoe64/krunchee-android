36e9979c7fc8fcf0074de40eb15b6e53e6050cb8
Jose Chavez <jose.chavez@dialexa.com>
2024-09-05 10:00:15 -0700

    Using Poppins font

--- app/src/main/java/com/rangerforce/krunchee/ui/theme/Type.kt
+++ app/src/main/java/com/rangerforce/krunchee/ui/theme/Type.kt
@@ -21,7 +21,7 @@ val bodyFontFamily = FontFamily(
 
 val displayFontFamily = FontFamily(
     Font(
-        googleFont = GoogleFont("Open Sans"),
+        googleFont = GoogleFont("Poppins"),
         fontProvider = provider,
     )
 )
