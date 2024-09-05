4672c3bf3100a08ba794dc247fa68dffd9a0d67a
Jose Chavez <jose.chavez@dialexa.com>
2024-09-05 09:26:44 -0700

    Added colors from logo

--- app/src/main/java/com/rangerforce/krunchee/ui/theme/Color.kt
+++ app/src/main/java/com/rangerforce/krunchee/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Krunchee logo colors
+val BlackBean = Color(0xFF260000)
+val PennRed = Color(0xFF920700)
+val ChiliRed = Color(0xFFDB1B0B)
+val NaplesYellow = Color(0xFFF6D85D)
+val Cream = Color(0xFFFFFEBA)
